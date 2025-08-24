94323278164ce65e705381579753baa82cc7419c
Youssef Abdelwahab <110339904+Youssef-Abdelwahab@users.noreply.github.com>
2025-08-24 15:52:47 +0300

    Add files via upload

---
+++ NewBot.py
@@ -0,0 +1,23 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, ContextTypes, filters
+
+async def unknown(update, context):
+    await update.message.reply_text(
+        "Unknown Command ⚠️"
+    )
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Hello {} 👋".format(update.message.from_user.first_name)
+    )
+
+def main():
+    application = Application.builder().token("8396089284:AAFm-ICZqIzQRh_wTCso_j8mfTy_HI9gZ7Q").build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(MessageHandler(filters.COMMAND | filters.TEXT, unknown))
+
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
